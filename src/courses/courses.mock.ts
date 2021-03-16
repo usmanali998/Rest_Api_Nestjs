a39be60e89f2f8d7705e78a5dc49b838644baede
usamapal34 <usamapal34@gmail.com>
2021-03-16 12:51:14 -0700

    rest apis using nestjs

---
+++ src/courses/courses.mock.ts
@@ -0,0 +1,6 @@
+export const COURSES = [
+    {id: 1, title: 'NodeJS - The complete guide (incl. MVC, Rest APIs, GraphQL', description: 'Master the NodeJS', author: 'Amir Savvy', url: 'https://google.com/' },
+    {id: 2, title: 'Python/Django - The complete guide (incl. MVC, Rest APIs, GraphQL', description: 'Master the Python/Django', author: 'Amir Savvy', url: 'https://google.com/' },
+    {id: 3, title: 'Python/Flask - The complete guide (incl. MVC, Rest APIs, GraphQL', description: 'Master the Python/Flask', author: 'Amir Savvy', url: 'https://google.com/' },
+    {id: 4, title: 'NestJS - The complete guide (incl. MVC, Rest APIs, GraphQL', description: 'Master the NestJS', author: 'Amir Savvy', url: 'https://google.com/' }
+]
